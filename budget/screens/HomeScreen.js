52812160ec39ddac295026593baf71a85fc77d38
William <billyreyes@rogers.com>
2018-10-01 18:41:06 -0400

    Added tip Calculator

--- budget/screens/HomeScreen.js
+++ budget/screens/HomeScreen.js
@@ -7,95 +7,73 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Button,
+  TextInput,
 } from 'react-native';
 import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
+import Transaction from '../components/Transaction';
 
 export default class HomeScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: "",
+      budget: 500,
+      bill: "0.00",
+      tip: "0.00",
+    }
+  }
   static navigationOptions = {
     header: null,
   };
+
+  setBudget = (value) =>{
+    this.setState({
+      input: value,
+    })
+  };
+  onPressBudget = () => {
+    this.setState({
+      budget: this.state.input,
+    })
+  };
+
 
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
           <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+            <Text style={styles.getStartedText}>Your Budget</Text>
+            <View style={styles.welcomeContainer}> 
+              <Text style={styles.budget}>${this.state.budget}</Text>
             </View>
 
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
+            <View>
+              <Text style={styles.calculatorTitle}>Enter Amount</Text>
+              <TextInput  style={styles.billAmount}  keyboardType='numeric' value={this.state.bill} onChangeText={ (value) => this.setState({bill:value})}/>
+              
+              <TouchableOpacity style={styles.button} onPress={ () => this.setState({tip: (this.state.bill * 0.10).toFixed(2)})}>
+                <Text style={styles.tipText}>10%</Text>
+              </TouchableOpacity>
 
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={ () => this.setState({tip: (this.state.bill * 0.12).toFixed(2)})}>
+                <Text style={styles.tipText}>12%</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.button} onPress={ () => this.setState({tip: (this.state.bill * 0.15).toFixed(2)})}>
+                <Text style={styles.tipText}>15%</Text>
+              </TouchableOpacity>
+
+              <Text style={styles.calculatorTitle}>Tip: ${this.state.tip}</Text>
+            </View>
+
           </View>
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -103,13 +81,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  tipText:{
+    fontSize:25,
+    color: 'white',
   },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#336EFF',
+    padding: 10,
+    height: 55,
+  },
+  billAmount:{
+    textAlign: 'center',
+    fontSize: 30,
+    padding: 10,
+  },
+  budget:{
+    fontSize: 75,
+    color: 'green',
+  },
+  calculatorTitle:{
+    fontSize:50,
+  },
+
   contentContainer: {
     paddingTop: 30,
   },
@@ -118,33 +112,9 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -171,18 +141,5 @@ const styles = StyleSheet.create({
     color: 'rgba(96,100,109, 1)',
     textAlign: 'center',
   },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+
 });
